b2e0bc2baa9e931554c3ad8cd72545e5b682af60
Denys Dovhan <email@denysdovhan.com>
2017-05-21 14:47:12 +0300

    Scaffold project

---
+++ app/renderer/app.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./style.css";
+
+// eslint-disable-next-line react/prefer-stateless-function
+export default class App extends React.Component {
+
+  static propTypes = {
+
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Hello!</h1>
+      </div>
+    );
+  }
+
+}
---
+++ stylelint.config.js
@@ -0,0 +1,12 @@
+"use strict";
+
+module.exports = {
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-css-modules"
+  ],
+  rules: {
+    "no-empty-source": null,
+    "selector-list-comma-newline-after": null
+  }
+};
