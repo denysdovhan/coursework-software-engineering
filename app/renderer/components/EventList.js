1684afa64691dcd25b9d2ad79dca7b84ef3607ba
Denys Dovhan <email@denysdovhan.com>
2017-05-28 21:24:48 +0300

    Show message when there is no events

--- app/renderer/components/EventList.js
+++ app/renderer/components/EventList.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { grey500 } from 'material-ui/styles/colors';
 import Event from './Event';
 
 const EventList = ({ events, toggleEventClick, updateEventClick, removeEventClick }) => {
   return (
     <ul>
-      {events.map(event =>
+      {events.length ? events.map(event => (
         <Event
           key={event.id}
           {...event}
           toggleEvent={() => toggleEventClick(event.id)}
           removeEvent={() => removeEventClick(event.id)}
           updateEvent={updated => updateEventClick(event.id, updated)} />
+      )) : (
+        <h1 style={{
+          textAlign: 'center',
+          marginTop: '1rem',
+          color: grey500,
+        }}>
+          There is no events…
+        </h1>
       )}
     </ul>
   );
